05ca5b23f64753c76cb2568cfb4e93bc622cbfc0
franco filippo <francofilip@hotmail.com>
2024-01-17 22:03:53 -0300

    nuevos cambios

---
+++ clase n3/js/index.js
@@ -0,0 +1,31 @@
+// ciclos 
+
+//ciclo por conteo (counted loop) = corresponde al ciclo FOR
+// ESTE CICLO ES CONTROLADO POR NOSOTROS YA QUE NOSOTROS LE EINDICAMOS DE ANTEMANO CUANTAS VECES QUEREMOS QUE SE EJECUTE
+
+//ciclos por condicioones (condicional loop) = ciclo while o do...while
+
+//ESTE CICLO NO ES CONTROLADO POR NOSOTROS, SI NO QUE RESPONDE A UNA CONDICION EN LA CUAL, SI SE CUMPLE TERMINA EL PROCESO.
+
+
+//operadores = 
+
+//operador de incremento = ++
+
+// let x = 5
+// let y =x++;
+// console.log(y)
+
+
+//post-incremento = variable++ ,  el valor de la variable se incrementa despues de que se utiliza la expresion actual
+
+//pre-incremento = ++variable , nos dice que el valor de la variable se incrementa antes de que se utilice
+
+//operador de incremento = --
+
+
+//ciclo FOR 
+
+
+
+
